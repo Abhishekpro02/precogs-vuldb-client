225b6f5be9f0392bbb1d9eb58c48c0fa1bb0313a
Abhishek Kumar <sahniak56@gmail.com>
2024-03-03 10:04:40 +0530

    add header login and signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./components/NotFound";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,189 @@
+import React, { useEffect, useState } from "react";
+import {
+  Navbar,
+  Typography,
+  Button,
+  IconButton,
+  Collapse,
+} from "@material-tailwind/react";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  const [openNav, setOpenNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <NavLink
+          to="/"
+          className="flex items-center"
+          onClick={() => setOpenNav(false)}
+          style={({ isActive }) => (isActive ? { color: "blue" } : null)}
+        >
+          Home
+        </NavLink>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <NavLink
+          to="/about"
+          className="flex items-center"
+          onClick={() => setOpenNav(false)}
+          style={({ isActive }) => (isActive ? { color: "blue" } : null)}
+        >
+          About
+        </NavLink>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <NavLink
+          to="/dashboard"
+          className="flex items-center"
+          onClick={() => setOpenNav(false)}
+          style={({ isActive }) => (isActive ? { color: "blue" } : null)}
+        >
+          Dashboard
+        </NavLink>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <NavLink
+          to="/profile"
+          className="flex items-center"
+          onClick={() => setOpenNav(false)}
+          style={({ isActive }) => (isActive ? { color: "blue" } : null)}
+        >
+          Profile
+        </NavLink>
+      </Typography>
+    </ul>
+  );
+
+  return (
+    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <Link to="/">
+          <Typography
+            as="div"
+            className="mr-4 cursor-pointer py-1.5 font-bold text-lg lg:mr-0 lg:py-0 lg:text-xl uppercase lg:cursor-auto"
+          >
+            Technical <span className="text-indigo-600">Abhi</span>
+          </Typography>
+        </Link>
+        <div className="flex items-center gap-4">
+          <div className="mr-4 hidden lg:block">{navList}</div>
+          <div className="flex items-center gap-x-1">
+            <Link to="/login">
+              <Button
+                variant="text"
+                size="sm"
+                className="hidden lg:inline-block"
+              >
+                <span>Sign In</span>
+              </Button>
+            </Link>
+            <Link to="/register">
+              <Button
+                variant="gradient"
+                size="sm"
+                className="hidden lg:inline-block"
+              >
+                <span>Sign up</span>
+              </Button>
+            </Link>
+          </div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
+        </div>
+      </div>
+      <Collapse open={openNav}>
+        {navList}
+        <div className="flex items-center gap-x-4 justify-center">
+          <Link to="/login">
+            <Button
+              fullWidth
+              variant="text"
+              size="sm"
+              className=""
+              onClick={() => setOpenNav(!openNav)}
+            >
+              <span>Sign In</span>
+            </Button>
+          </Link>
+          <Link to="/register">
+            <Button
+              fullWidth
+              variant="gradient"
+              size="sm"
+              className=""
+              onClick={() => setOpenNav(!openNav)}
+            >
+              <span>Sign up</span>
+            </Button>
+          </Link>
+        </div>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
