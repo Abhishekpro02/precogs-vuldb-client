512cc96812f3cd318c3a4aacedc7cc948752b6f9
Abhishek Kumar <sahniak56@gmail.com>
2024-03-04 21:56:23 +0530

    add profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(async () => {
+    const base_url = "https://precogs-vuln-db-uvq3.vercel.app/api/v1";
+    const res = await axios.get(`${base_url}/profile`);
+    setUser(res.data.user);
+  }, [user]);
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="text-2xl font-bold">
         This is the <span className="text-indigo-500">Profile</span> Page
       </div>
+      <pre>{JSON.stringify(user, null, 4)}</pre>
     </div>
   );
 };
