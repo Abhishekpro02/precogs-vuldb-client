748635f1ec6ef1566d1501ed0d5beaab15ff4082
Abhishek Kumar <sahniak56@gmail.com>
2024-03-05 00:54:29 +0530

    hgh

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,11 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const About = () => {
-  const [user, setUser] = useState(null);
-
-  useEffect(async () => {
-    try {
-      const base_url = "https://precogs-vuln-db-uvq3.vercel.app/api/v1";
-      const res = await axios.get(`${base_url}/profile`);
-      setUser(res.data.user);
-    } catch (error) {
-      console.error("Error fetching user:", error);
-    }
-  }, [user]);
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className=" text-2xl font-bold">
         This is the <span className="text-indigo-500">About us</span> Page
       </div>
-      <pre>{JSON.stringify(user, null, 4)}</pre>
     </div>
   );
 };
