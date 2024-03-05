d1f5dc3f285794ee6137dc9ae1f653bb6b6247f4
Abhishek Kumar <sahniak56@gmail.com>
2024-03-05 10:51:22 +0530

    jkb

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -18,7 +18,6 @@ const Dashboard = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <UserCard user={auth.user} />
-      <h1>db</h1>
     </div>
   );
 };
