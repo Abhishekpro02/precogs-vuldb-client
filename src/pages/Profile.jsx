f358b5253a0bfd7cd75711bc3b8ac598dd5b6610
Abhishek Kumar <sahniak56@gmail.com>
2024-03-05 10:14:35 +0530

    jgf

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -11,7 +11,7 @@ const Profile = () => {
     });
     console.log(res);
     setUser(res.data.user);
-  }, []);
+  }, [user, setUser]);
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="text-2xl font-bold">
