934e1082c1deb4a89482a249d3808ebbe712f9b1
Abhishek Kumar <sahniak56@gmail.com>
2024-03-05 10:53:57 +0530

    jbk

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
       setAuth({
         ...auth,
         user: response.data.user,
-        token: response.data.accessToken,
+        token: "uhkuh",
       });
 
       localStorage.setItem("auth", JSON.stringify(response.data));
